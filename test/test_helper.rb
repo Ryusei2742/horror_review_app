1dc9f0f330b8c3559ea6df063a9c148fd3d27bbd
ryusei42 <ryuseitakata27@gmail.com>
2023-04-06 15:18:38 +0900

    Add a full_title helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,11 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
+  # 指定のワーカー数でテストを並列実行する
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
