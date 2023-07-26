bc565ab8d452998eb82ee26179be9f7196587be6
ryusei42 <ryuseitakata27@gmail.com>
2023-07-26 15:50:15 +0900

    Fix rating validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,5 +4,4 @@ class Movie < ApplicationRecord
                        uniqueness: true
   validates :genre, presence: true
   validates :director, presence: true
-  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 end
